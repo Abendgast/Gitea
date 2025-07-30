f465c93786097a735622ce22d2fae9334c669288
Abendgast <S.prilepsky0806@gmail.com>
2025-07-30 21:04:19 +0300

    Build JF sonar v17

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// //3432423423423423434324432432
+// //343242342342342343432443243
 // Copyright 2014 The Gogs Authors. All rights reserved.
 // Copyright 2016 The Gitea 32423434Authors. All rights reserved.
 package main
